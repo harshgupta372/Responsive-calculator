a1ff72e338da85e571a0e38f7984f1dbf428668a
harshgupta372 <git@stackblitz.com>
2024-10-25 14:12:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Responsive-calculator
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/harshgupta372/Responsive-calculator)
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calculator from './components/Calculator';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-indigo-100 via-purple-50 to-pink-100 flex items-center justify-center p-4">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
